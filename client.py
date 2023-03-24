885905bce3f1f9f1bd64a67f7ff150d50915aa0a
panvong <128306930+panvong@users.noreply.github.com>
2023-03-23 23:32:07 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -12,15 +12,27 @@ port = 9999
 clientsocket.connect((host, port))
 
 # send data to the server
-data = 10
-message = str(data).encode('utf-8')
-clientsocket.sendall(message)
+micro_data = 10
+message1 = str(micro_data).encode('utf-8')
+clientsocket.sendall(message1)
+
 
 # receive data from the server
-data = clientsocket.recv(1024)
-result = int(data.decode('utf-8'))
+micro_data = clientsocket.recv(1024)
+result1 = int(micro_data.decode('utf-8'))
 
-print("Result: {}".format(result))
+print("Result1: {}".format(result1))
+
+macro_data = 5
+message2 = str(macro_data).encode('utf-8')
+clientsocket.sendall(message2)
+
+
+# receive data from the server
+macro_data = clientsocket.recv(1024)
+result2 = int(macro_data.decode('utf-8'))
+
+print("Result2: {}".format(result2))
 
 # close the client socket
 clientsocket.close()
--- server.py
+++ server.py
@@ -23,14 +23,19 @@ while True:
     print("Got a connection from {}".format(addr))
 
     # receive data from the client
-    data = clientsocket.recv(1024)
-
+    micro_data = clientsocket.recv(1024)
     # add 5 to the data
-    result = int(data.decode('utf-8')) + 5
-
+    result1 = int(micro_data.decode('utf-8')) + 5
     # send the result back to the client
-    message = str(result).encode('utf-8')
-    clientsocket.sendall(message)
+    message1 = str(result1).encode('utf-8')
+    clientsocket.sendall(message1)
+
+    macro_data = clientsocket.recv(1024)
+    # add 7 to the data
+    result2 = int(macro_data.decode('utf-8')) + 7
+    # send the result back to the client
+    message2 = str(result2).encode('utf-8')
+    clientsocket.sendall(message2)
 
     # close the client socket
     clientsocket.close()
